da39e15e0bb2744a0ca3c53d436db2459adc5786
IfalEXZ <ifaltampan69@gmail.com>
2024-11-02 21:46:52 +0800

    add react-markdown for md support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { requestToGroqAI } from "./utils/groq"
-
+import Markdown from 'react-markdown'
 
 function App() {
   const [data, setData] = useState("")
@@ -10,7 +10,7 @@ function App() {
   }
   return (
     <div className="flex flex-col min-h-[100vh] justify-center items-center w-full">
-      <h1 className="text-4xl text-white">GROQ Test</h1>
+      <h1 className="text-4xl text-white font-bold m-2">GROQ Test</h1>
       <form className="flex flex-col gap-4 py-4">
         <input
          className="py-2 px-4 text-md rounded-md text-black" type="text"
@@ -18,7 +18,9 @@ function App() {
          id="content" />
         <button onClick={handleSubmit} type="button" className="bg-indigo-500 rounded-md py-2 px-4 font-bold text-white">Send</button>
       </form>
-      <div>{data}</div>
+      <div className="text-white w-3/4 bg-gray-800 p-8 rounded break-words text-wrap">
+        <Markdown>{data}</Markdown>
+        </div>
     </div>
   )
 }
